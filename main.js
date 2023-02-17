43a8560d769a015c3d58b4aecb89e4e5cc4c074e
jared <jaredgold@proton.me>
2023-02-17 17:16:56 -0500

    create gameBoardStack holding gameboard functions

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 (function game() {
-  const gameBoard = {
-    gameArray: ['x','x','x','x','x','x','x','x','x']
-  }
-
-  const gameBoardCreate = (() => {
-    const gameBoardContainer = document.querySelector('.board-container');
-    for(let i = 1; i < 10; i++){
-      const gameSquare = document.createElement('div');
-      gameSquare.classList.add('game-square');
-      gameSquare.dataset.value = [i];
-      gameBoardContainer.appendChild(gameSquare);
+  const gameBoardStack = (() => {
+    const gameBoard = {
+      gameArray: ['x','x','x','x','x','x','x','x','x']
     }
-  })();
 
+    function gameBoardCreate() {
+      const gameBoardContainer = document.querySelector('.board-container');
+      for(let i = 1; i < 10; i++){
+        const gameSquare = document.createElement('div');
+        gameSquare.classList.add('game-square');
+        gameSquare.dataset.value = [i];
+        gameBoardContainer.appendChild(gameSquare);
+      }
+    };
+
+    return {gameBoard, gameBoardCreate};
+  })();
+  
   const playerControl = (() => {
     //could add a prompt here to modify the player names
     const players = {
