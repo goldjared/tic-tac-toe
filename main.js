24d44693df418135d895fbaf0811446f3185fb9c
jared <jaredgold@proton.me>
2023-02-18 19:42:27 -0500

    create play function, calls evaluator, eval func runs loop checking answer key

--- main.js
+++ main.js
@@ -26,6 +26,59 @@
     return players
   })();
 
+  function gamePlay(){
+    if(playerControl.player1 != '' && playerControl.player2 != '') {
+      // let answer = prompt('play game?', '');
+      // if(answer==='yes') {
+        // gameBoardStack.gameBoardCreate();
+        function insertValue(target, entry) {
+          //get target index, and number to go in there
+
+          game.gameBoardStack.gameBoard.gameArray[target] = entry;
+          gameEvaluator();
+        // }
+      }
+      return {insertValue};
+     };
+  };
+
+  const gameEvaluator = () => {
+    // game.gameBoardStack.gameBoard.gameArray.forEach(entry => )
+    
+    const answerKey = {
+      1: [0,1,2],
+      2: [3,4,5],
+      3: [6,7,8],
+      4: [0,3,6],
+      5: [1,4,7],
+      6: [2,5,8],
+      7: [0,4,8],
+      8: [6,4,2]
+      // 1: [[0],[1],[2]],
+      // 2: [[3],[4],[5]],
+      // 3: [[6],[7],[8]],
+      // 4: [[0],[3],[6]],
+      // 5: [[1],[4],[7]],
+      // 6: [[2],[5],[8]],
+      // 7: [[0],[4],[8]],
+      // 8: [[6],[4],[2]]
+    }
+
+    for(let i = 1; i < 9; i++){
+      // console.log(answerKey[i]);
+      let evaluation = answerKey[i].map
+      (i => game.gameBoardStack.gameBoard.gameArray[i]);
+      console.log(evaluation);
+    }
+    // console.log(game.gameBoardStack.gameBoard.gameArray);
+    
+    // console.log(Object.keys(answerKey).length);
+    
+    return answerKey;
+  }
+
+  return {gameBoardStack, playerControl, gamePlay, gameEvaluator};
+
   
   
 
