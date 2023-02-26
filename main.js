023f07b08a9c8d8633e2b1a0d3733b7c990d5f33
jared <jaredgold@proton.me>
2023-02-26 18:55:12 -0500

    add gameEnd func, reset program at end. remove debug/format code

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 const game = (() => {
-  const gameBoardStack = (() => {
+  const boardStack = (() => {
     const gameBoard = {
-      gameArray: [null, null, null, null, null, null, null, null, null]
+      gameArray: [null, null, null, 'o', 'o', null, null, null, null]
+      //gameArray: ['x', 'k', 'a', 'a', 'f', 'j', 'k', null, null]
     }
 
     function gameBoardCreate() {
       const gameBoardContainer = document.querySelector('.board-container');
-      for(let i = 1; i < 10; i++){
-        const gameSquare = document.createElement('div');
-        gameSquare.classList.add('game-square');
-        gameSquare.dataset.value = [i];
-        gameBoardContainer.appendChild(gameSquare);
-      }
+      // for(let i = 1; i < 10; i++){
+      //   const gameSquare= [i];
+      //   console.log(gameSquare)
+      //   // const gameSquare = document.createElement('div');
+      //   // gameSquare.classList.add('game-square');
+      //   // gameSquare.dataset.value = [i];
+      //   // gameBoardContainer.appendChild(gameSquare);
+      // }
     };
 
     return {gameBoard, gameBoardCreate};
   })();
 
   const playerControl = (() => {
-    const player1 = makePlayer(prompt('Player, enter name, and weapon (name, x/o)')
-    .split(','));
-    const player2 = makePlayer(prompt('Player, enter name, and weapon (name, x/o)')
-    .split(','));
+    // const player1 = makePlayer(prompt('Player, enter name, and weapon (name, x/o)')
+    // .split(','));
+    // const player2 = makePlayer(prompt('Player, enter name, and weapon (name, x/o)')
+    // .split(','));
+    const player1 = {
+      name: 'john',
+      selection: 'x'
+    };
+    const player2 = {
+      name: 'doe',
+      selection: 'o'
+    };
 
-    while(Object.keys(players).length < 2) {
-    function makePlayer(entry){
-      return {
-        name: entry[0],
-        selection: entry[1]
-      }
-      let playerEntry = prompt('Player, enter name, and weapon (name, x/o)')
-      .split(',');
-      makePlayer(playerEntry);
-    }
-    
-    //     players[entry[0]] = entry[1];
+    // function makePlayer(entry){
+    //   return {
+    //     name: entry[0],
+    //     selection: entry[1]
     //   }
-    //   // let playerEntry = prompt('Player, enter name, and weapon (name, x/o)')
-    //   // .split(',');
-    //   // makePlayer(playerEntry);
     // }
     
     return {player1, player2}
@@ -75,15 +75,12 @@ const game = (() => {
         currentPlayer.selection; //make thiss current player turn
         console.log('insertval log, this is array1', game.boardStack.gameBoard.gameArray)
         gameEvaluator();
-        // }
       }
       return {insertValue};
      };
   };
 
   const gameEvaluator = () => {
-    // game.gameBoardStack.gameBoard.gameArray.forEach(entry => )
-    
     const answerKey = {
       1: [0,1,2],
       2: [3,4,5],
@@ -93,22 +90,12 @@ const game = (() => {
       6: [2,5,8],
       7: [0,4,8],
       8: [6,4,2]
-      // 1: [[0],[1],[2]],
-      // 2: [[3],[4],[5]],
-      // 3: [[6],[7],[8]],
-      // 4: [[0],[3],[6]],
-      // 5: [[1],[4],[7]],
-      // 6: [[2],[5],[8]],
-      // 7: [[0],[4],[8]],
-      // 8: [[6],[4],[2]]
     }
 
     for(let i = 1; i < 9; i++){
-      // console.log(answerKey[i]);
       let evaluation = answerKey[i].map
       (i => game.boardStack.gameBoard.gameArray[i]);
       isEvalutionEqual(evaluation);
-
     }
 
     function isEvalutionEqual(array){
@@ -117,30 +104,38 @@ const game = (() => {
       const isMatchingKey = (currentValue) => 
       currentValue === array[0] && currentValue != null;
 
+      let result = '';
+
       if(game.boardStack.gameBoard.gameArray.length > 7 &&
         game.boardStack.gameBoard.gameArray
         .every(noNullValue) === true &&
         array.every(isMatchingKey) === false) {
         console.log('draw');
+        result = 'draw';
       }
-      // console.log(array.every(isMatchingKey));
+
       if(array.every(isMatchingKey) === true){
         console.log(array, 'winner');
+        result = 'win';
         (array[0] === playerControl.player1.selection) 
         ? console.log(`${playerControl.player1.name} WON!`) 
         : console.log(`${playerControl.player2.name} WON!`);
-        //check array[0] = p1 or p2.selection. announce player.
-
+        gameEnd(result);
+        
       };
-    }
-    return isEvalutionEqual;
-  }
+    };
+
+    function gameEnd(result) {
+        game.boardStack.gameBoard.gameArray = [null, null, null, null, null, null, null, null, null];
+        game.playerTurn = turnCreation();
+        console.log('result', result);
+        // isEvalutionEqual.result = '';
+        console.log('this is gameEnd log', game.boardStack.gameBoard.gameArray);
+        console.log('this is gameEnd log', playerTurn.getTurn());
+      }
+    return {isEvalutionEqual};
+  };
 
   return {boardStack, playerControl, gamePlay, 
-    gameEvaluator};
-
-  
-  
-
-  
+  gameEvaluator};
 })();
